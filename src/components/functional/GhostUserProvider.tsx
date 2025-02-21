e7a67636238b14dc85af032fa903154243a3ccbc
Rafael Calpena Rodrigues <rafael.calpena@gmail.com>
2025-02-21 12:20:40 -0300

    fix: use isLoadingUser true by default

--- src/components/functional/GhostUserProvider.tsx
+++ src/components/functional/GhostUserProvider.tsx
@@ -38,7 +38,7 @@ export const UserProvider = ({ children }: ChildrenProps) => {
     }>({
         user: undefined,
         isErrorUser: undefined,
-        isLoadingUser: false
+        isLoadingUser: true
     });
 
     useEffect(() => {
