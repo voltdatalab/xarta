97e3c25765e98a6c2c48678b151fbb74c2c0305a
Rafael Calpena Rodrigues <rafael.calpena@gmail.com>
2025-02-21 12:02:30 -0300

    fix: attempt to fix unnecessary redirection bug

--- src/components/functional/GhostUserProvider.tsx
+++ src/components/functional/GhostUserProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { ChildrenProps } from '@/lib/utils';
 import { PUBLIC_NEXT_API_BASE_URL } from '@/config/config';
@@ -26,10 +26,47 @@ export const useGhostUser = () => {
 // UserProvider component to fetch user data and provide it to children
 export const UserProvider = ({ children }: ChildrenProps) => {
     // Fetch user data using TanStack Query
-    const { data: user, error: isErrorUser, isLoading: isLoadingUser } = useQuery({
-        queryKey: ['user'],
-        queryFn: fetchGhostUser
+    // const { data: user, error: isErrorUser, isLoading: isLoadingUser } = useQuery({
+    //     queryKey: ['user'],
+    //     queryFn: fetchGhostUser
+    // });
+
+    const [{user, isLoadingUser, isErrorUser}, setUser] = useState<{
+        user: any,
+        isErrorUser: any,
+        isLoadingUser: any
+    }>({
+        user: undefined,
+        isErrorUser: undefined,
+        isLoadingUser: false
     });
+
+    useEffect(() => {
+        setUser({
+            user: undefined,
+            isErrorUser: false,
+            isLoadingUser: true
+        })
+        fetchGhostUser().then(data => {
+            setUser({
+                user: data,
+                isErrorUser: false,
+                isLoadingUser: false
+            })
+        })
+        .catch(() => {
+            setUser({
+                user: undefined,
+                isErrorUser: true,
+                isLoadingUser: false
+            })
+        })
+    }, [])
+
+    // const { data: user, error: isErrorUser, isLoading: isLoadingUser } = useQuery({
+    //     queryKey: ['user'],
+    //     queryFn: fetchGhostUser
+    // });
 
     const {data: settings } = useQuery({
         queryKey: ['settings'],
