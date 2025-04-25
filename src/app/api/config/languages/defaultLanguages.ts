af198550be67a25c2337ef12177a0c21fc38d049
Rafael Calpena Rodrigues <rafael_calpena@hotmail.com>
2025-04-24 23:36:42 -0300

    feat: add spanish translation

--- src/app/api/config/languages/defaultLanguages.ts
+++ src/app/api/config/languages/defaultLanguages.ts
@@ -1,6 +1,7 @@
 export const defaultLanguages = [
   { code: 'en', name: 'English' },
-  { code: 'pt', name: 'Português' }
+  { code: 'pt', name: 'Português' },
+  { code: 'es', name: 'Español' }
 ];
 
 export const defaultCode = 'en';
